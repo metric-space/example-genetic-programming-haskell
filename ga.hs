b175cf482b082d1c8d0ad7ac52b4efd9d5b4478f
luke <nerorevenge@gmail.com>
2017-01-15 03:47:44 -0500

    tree zippers now in place  - Tree mutation is also in place

--- ga.hs
+++ ga.hs
@@ -2,16 +2,39 @@ import Control.Monad.Trans.State
 import System.Random
 
 data Variable = X | Y deriving (Show, Enum)
+data Operation = Mult | Div | Add | Sub deriving (Show, Enum)
 
 data Expr = Value Variable 
         | Const Float
-	| Mult Expr Expr 
-	| Div Expr Expr 
-	| Add Expr Expr
-	| Sub Expr Expr deriving (Show)
+	| Op Operation Expr Expr deriving (Show)
 
-functions :: [Expr -> Expr -> Expr]
-functions = [Mult, Div, Add, Sub]
+-- taken from LYAH
+data Crumb = LeftCrumb Operation Expr | RightCrumb Operation Expr deriving (Show)
+type Zipper = (Expr,[Crumb])
+
+goLeft :: Zipper -> Maybe Zipper
+goLeft (Op x l r, bs) = Just (l, (LeftCrumb x r):bs)
+goLeft (Value _, _) = Nothing
+goLeft (Const _, _) = Nothing
+
+goRight :: Zipper -> Maybe Zipper
+goRight (Op x l r, bs) = Just (r, (RightCrumb x l):bs)
+goRight (Value _, _) = Nothing
+goRight (Const _, _) = Nothing
+
+goTop :: Zipper -> Maybe Zipper
+goTop (t, (LeftCrumb f r):bs) = Just (Op f t r, bs)
+goTop (t, (RightCrumb f l):bs) = Just (Op f l t, bs )
+goTop (_, []) = Nothing
+
+modifySubTree :: Expr -> Zipper -> Zipper
+modifySubTree a (_, bs) = (a, bs)  
+
+upMost :: Zipper -> Maybe Expr
+upMost (t, []) = Just t
+upMost x = goTop x >>= upMost
+
+-- LYAH 
 
 constructRandomTree :: Int -> State StdGen Expr 
 constructRandomTree 0 = do
@@ -31,25 +54,41 @@ constructRandomTree x = do
                            gen <- get
                            let (pick, newGen) = randomR (0,3) gen
                            put newGen 
-                           let start = functions !! pick
+                           let start = (toEnum pick :: Operation)
                            left <- constructRandomTree (x-1) 
                            right <- constructRandomTree (x-1)
-                           return $ start left right
+                           return $ Op start left right
                             
-
+-- have to make this shorter
 eval :: Expr -> Float -> Float -> Float
 eval (Const z) _ _ = z
 eval (Value X) x _ = x
 eval (Value Y) _ y = y
-eval (Div left right) x y = (/) (eval left x y) (eval right x y)
-eval (Sub left right) x y = (-) (eval left x y) (eval right x y)
-eval (Mult left right) x y = (*) (eval left x y) (eval right x y)
-eval (Add left right) x y = (+) (eval left x y) (eval right x y)
+eval (Op Div left right) x y = (/) (eval left x y) (eval right x y)
+eval (Op Sub left right) x y = (-) (eval left x y) (eval right x y)
+eval (Op Mult left right) x y = (*) (eval left x y) (eval right x y)
+eval (Op Add left right) x y = (+) (eval left x y) (eval right x y)
 
 
-
-
-
+-- gotta refactor this whole thing, look into monad transformers
+mutateTreeZipper :: Zipper -> State StdGen (Maybe Expr)
+mutateTreeZipper x = do
+                       gen <- get 
+                       let (pick, newGen) = randomR (0.0,1.0) gen :: (Float, StdGen)
+                       put newGen
+                       -- should change this so that the probability is very less for 
+                       -- upper levels
+                       if (pick > 0.70)
+	                 then do
+                               let newTree = evalState (constructRandomTree 2) newGen
+                               return $ upMost $ modifySubTree newTree x                   
+                         else do
+                                -- gotta refactor this
+                                let (pick, newGen2) = randomR (0,1) newGen :: (Int, StdGen)
+                                put newGen2
+                                case ([goLeft, goRight]!!pick) x of Just t -> mutateTreeZipper t
+                                                                    Nothing -> return Nothing
+                                                                  
                               
                               
 
