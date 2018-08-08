7e62f07404b9725a0585ff1ff6ffe1bdce723d81
luke <nerorevenge@gmail.com>
2018-08-08 18:00:41 -0400

    Linting Utils.hs

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,19 +1,14 @@
-module Utils (
- selectIndex ,
- defaultENV
-) where
-
+module Utils
+  ( selectIndex
+  , defaultENV
+  ) where
 
 import System.Random (randomIO)
 
 import Types (ENV(..), STreeENV(..))
 
-
 selectIndex :: Double -> IO Int
-selectIndex pexp = randomIO >>= (return . round . flip (/) (log pexp) . log)
-
+selectIndex pexp = (round . flip (/) (log pexp) . log) <$> randomIO
 
 defaultENV :: ENV
 defaultENV = ENV 0.1 0.4 500 500 0.7 0.05 (STreeENV 4 0.5 0.6) (const 1000000)
-
-
