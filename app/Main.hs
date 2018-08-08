a6a77010772a1f4ae0eb693c385ac30c31b4899d
luke <nerorevenge@gmail.com>
2018-08-08 17:56:08 -0400

    linting Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,43 +1,36 @@
-module Main (main) where
+module Main
+  ( main
+  ) where
 
 import Data.Map (Map, fromList)
 import System.Random (randomRIO)
 
-import Tree (evalSTree)
-import Types (STree, P(..), evalFunction)
 import Evolve (evolve)
+import Tree (evalSTree)
+import Types (P(..), STree, evalFunction)
 import Utils (defaultENV)
 
 -- ======================    The problem  ===================================================
-
 hiddenFunction :: Int -> Int -> Int
-hiddenFunction x y = x*x + 2*y + 3*x + 5 
+hiddenFunction x y = x * x + 2 * y + 3 * x + 5
 
 -- ====================== FITNESS ======================================================
-
-
 buildSingleSet :: IO (Map P Int, Int)
 buildSingleSet = do
-                  x <- randomRIO (0,40)
-                  y <- randomRIO (0,40)
-                  return (fromList [(X, x), (Y, y)], hiddenFunction x y)
-
+  x <- randomRIO (0, 40)
+  y <- randomRIO (0, 40)
+  return (fromList [(X, x), (Y, y)], hiddenFunction x y)
 
 buildHiddenSet :: IO [(Map P Int, Int)]
-buildHiddenSet = traverse (const buildSingleSet) [1..200]
+buildHiddenSet = traverse (const buildSingleSet) [1 .. 200]
 
-
-
-evaluateFitness :: STree -> [(Map P Int,Int)] -> Int
-evaluateFitness tree lm = foldl1 (+) . map (\(m,value) -> abs . (-) value . evalSTree tree $ m) $ lm
-                  
+evaluateFitness :: STree -> [(Map P Int, Int)] -> Int
+evaluateFitness tree =
+  sum . map (\(m, value) -> abs . (-) value . evalSTree tree $ m)
 
 -- ===================== main prog ==============================================================
-
-
 main :: IO ()
 main = do
-        ms <- buildHiddenSet
-        let def = defaultENV {evalFunction = flip evaluateFitness ms }
-        evolve ms def 
- 
+  ms <- buildHiddenSet
+  let def = defaultENV {evalFunction = flip evaluateFitness ms}
+  evolve ms def
